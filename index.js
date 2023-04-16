ee7c39627b427c297f543d6588e28cbb444ce2f2
Ralph Nicholson <nicholson.re@gmail.com>
2023-04-15 19:08:10 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(season => season.result === 'W');
+    if (winningYear) {
+        return winningYear.year;
+    } else {
+        return undefined;
+    }
+}
